05ee66ed0032831abf88419d64b09f1ba1027072
RTijerina <126622300+RTijerina@users.noreply.github.com>
2024-04-12 17:25:03 -0600

    CRUD Roles

--- database/migrations/2024_03_08_050902_create_roles_table.php
+++ database/migrations/2024_03_08_050902_create_roles_table.php
@@ -32,7 +32,7 @@ return new class extends Migration
         });
 
         Schema::table('users', function (Blueprint $table) {
-            $table->foreign('role_id')->references('id')->on('roles');
+            $table->foreign('role_id')->references('id')->on('roles')->onDelete('cascade');
         });
     }
 
