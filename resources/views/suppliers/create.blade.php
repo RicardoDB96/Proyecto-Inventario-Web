1e23344eb64901f3dc2f3fde8ea24f30ea784066
joelaldanaa <143925955+joelaldanaa@users.noreply.github.com>
2024-04-15 15:19:13 -0600

    Create design modified de category y product

--- resources/views/suppliers/create.blade.php
+++ resources/views/suppliers/create.blade.php
@@ -6,42 +6,45 @@
 </div>
 
 <div class="place">
-    <a href="{{route('suppliers.index')}}" class="linkButton"><button class="button">VOLVER</button></a>
+    <a href="{{ route('suppliers.index') }}" class="linkButton"><button class="button">VOLVER</button></a>
 </div>
 
 <div class="container mt-4">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
+            <div class="card" style="background-color: #E9E6DD;">
+                <div class="card-header d-flex justify-content-start" style="background-color: #C4AD9D;">
+                    <h4 class="mb-0">ADD NEW SUPPLIER</h4>
+                </div>
                 <div class="card-body">
-                    <form action="{{route('suppliers.store')}}" method="POST">
+                    <form action="{{ route('suppliers.store') }}" method="POST">
                         @csrf
 
                         <div class="form-group row">
                             <label for="name" class="col-md-4 col-form-label text-md-right">Name:</label>
                             <div class="col-md-6">
-                                <input type="text" name="name" class="form-control" placeholder="Supplier Name">
+                                <input type="text" name="name" class="form-control" placeholder="Supplier Name" style="background-color: #D7D7D7;">
                             </div>
                         </div>
 
                         <div class="form-group row">
                             <label for="address" class="col-md-4 col-form-label text-md-right">Address:</label>
                             <div class="col-md-6">
-                                <textarea class="form-control" style="height:150px" name="address" placeholder="Address..."></textarea>
+                                <textarea class="form-control" style="height:150px; background-color: #D7D7D7;" name="address" placeholder="Address..."></textarea>
                             </div>
                         </div>
 
                         <div class="form-group row">
                             <label for="contact_phone" class="col-md-4 col-form-label text-md-right">Phone Number:</label>
                             <div class="col-md-6">
-                                <input type="text" name="contact_phone" class="form-control" placeholder="Phone Number">
+                                <input type="text" name="contact_phone" class="form-control" placeholder="Phone Number" style="background-color: #D7D7D7;">
                             </div>
                         </div>
 
                         <div class="form-group row">
                             <label for="is_active" class="col-md-4 col-form-label text-md-right">Status:</label>
                             <div class="col-md-6">
-                                <select name="is_active" class="form-select" id="">
+                                <select name="is_active" class="form-select" id="" style="background-color: #D7D7D7;">
                                     <option value="">-- Elige el status --</option>
                                     <option value="1">Activo</option>
                                     <option value="0">Inactivo</option>
@@ -61,3 +64,4 @@
     </div>
 </div>
 @endsection
+
